c10b0eb7d17c3d4edc36190d283e6a1d29551a84
danie <danie@DESKTOP-2JINL1V>
2022-07-03 16:42:10 -0300

    Adicionar arquivos de projeto.

---
+++ Quarto Exercicio Prova do Djalma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quarto_Exercicio_Prova_do_Djalma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
